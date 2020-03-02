5c6a96be2103bd6f8531ebf3f0dd416ece8db509
Opdata <pb03641@gmail.com>
2020-03-03 05:08:54 +0900

    [fix] /about 요청시 두개의 컴포넌트가 그려지는 문제를 수정
    exact props를 true로 설정

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { Route } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 
+// "/about" 요청시 두개의 컴포넌트가 둘다 그려진다.
+// 이를위해 Home을 위한 Route 컴포넌트를 사용할 때 exact라는 props를 true로 설정해주어야 한다.
+// exact={true}
+
 const App = () => {
   return (
     <div>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
     </div>
   );
